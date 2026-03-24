048b2a4d8d31931314e290fdc03398106d2d0adb
rencaiji2 <95458901+rencaiji2@users.noreply.github.com>
2026-03-24 17:01:10 +0800

    新版本协议

--- RGA-AnalogMachine/widget.cpp
+++ RGA-AnalogMachine/widget.cpp
@@ -11,11 +11,11 @@ Widget::Widget(QWidget *parent)
     m_tcpServer_2.startServer("127.0.0.1",6001);
     m_sendTimer = new QTimer(this);
     connect(m_sendTimer, &QTimer::timeout, this, &Widget::on_sendTimer_out, Qt::UniqueConnection);
-    m_sendTimer->start(100);
+    m_sendTimer->start(1000);
 
     m_testTimer = new QTimer(this);
     connect(m_sendTimer, &QTimer::timeout, this, &Widget::on_testTimer_out, Qt::UniqueConnection);
-    //m_testTimer->start(100);
+    m_testTimer->start(100);
 }
 
 Widget::~Widget()
@@ -45,14 +45,20 @@ void Widget::on_testTimer_out()
 
 void Widget::on_sendTimer_out()
 {
-    if(!m_tcpServer.getClientIds().isEmpty())
-    {
-        if(ui->cb_useUIsetting->isChecked())
-        {
-            handleUiSetting();
-        }
-        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1.join(","));
-    }
+//    static int sendCount = 0;
+//    sendCount ++;
+//    if(sendCount>=20)
+//    {
+//        m_sendTimer->stop();
+//    }
+//    if(!m_tcpServer.getClientIds().isEmpty())
+//    {
+//        if(ui->cb_useUIsetting->isChecked())
+//        {
+//            handleUiSetting();
+//        }
+//        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1.join(","));
+//    }
     if(!m_tcpServer_2.getClientIds().isEmpty())
     {
         m_tcpServer_2.sendMessage(m_tcpServer.getClientIds()[0],"SV,1,2,3,4,5,6,7,8,3,3,11,12,13,14,15,16,17,18");
@@ -78,7 +84,7 @@ void Widget::handleUiSetting()
 void Widget::initTcpData_1()
 {
     tcpData_1.clear();
-    for (int i=0;i<300;i++) {
+    for (int i=0;i<52;i++) {
         tcpData_1.append("N");
     }
     tcpData_1[0] = "RND";
@@ -86,26 +92,292 @@ void Widget::initTcpData_1()
     tcpData_1[2] = "1";
     tcpData_1[23] = "1";
     tcpData_1[24] = "1";
+    tcpData_1[51] = "\r\n";
+
+    tcpData_1_2.clear();
+    for (int i=0;i<59;i++) {
+        tcpData_1_2.append("N");
+    }
+    tcpData_1_2[0] = "RND";
+    tcpData_1_2[1] = "1";
+    tcpData_1_2[2] = "1";
+    tcpData_1_2[23] = "1";
+    tcpData_1_2[24] = "1";
+    tcpData_1_2[58] = "\r\n";
 }
 
 
 void Widget::on_pushButton_clicked()
 {
-    tcpData_1[252] = ui->lineEdit_CH1_Lot->text();
-    tcpData_1[253] = ui->lineEdit_CH1_SLot->text();
+//    tcpData_1[252] = ui->lineEdit_CH1_Lot->text();
+//    tcpData_1[253] = ui->lineEdit_CH1_SLot->text();
 
-    tcpData_1[257] = ui->lineEdit_CH3_Lot->text();
-    tcpData_1[258] = ui->lineEdit_CH3_SLot->text();
+//    tcpData_1[257] = ui->lineEdit_CH3_Lot->text();
+//    tcpData_1[258] = ui->lineEdit_CH3_SLot->text();
 
-    tcpData_1[262] = ui->lineEdit_CH4_Lot->text();
-    tcpData_1[263] = ui->lineEdit_CH4_SLot->text();
+//    tcpData_1[262] = ui->lineEdit_CH4_Lot->text();
+//    tcpData_1[263] = ui->lineEdit_CH4_SLot->text();
 
-    tcpData_1[267] = ui->lineEdit_CH6_Lot->text();
-    tcpData_1[268] = ui->lineEdit_CH6_SLot->text();
+//    tcpData_1[267] = ui->lineEdit_CH6_Lot->text();
+//    tcpData_1[268] = ui->lineEdit_CH6_SLot->text();
 
-    tcpData_1[272] = ui->lineEdit_LLA_Lot->text();
-    tcpData_1[273] = ui->lineEdit_LLA_SLot->text();
+//    tcpData_1[272] = ui->lineEdit_LLA_Lot->text();
+//    tcpData_1[273] = ui->lineEdit_LLA_SLot->text();
 
-    tcpData_1[277] = ui->lineEdit_LLB_Lot->text();
-    tcpData_1[278] = ui->lineEdit_LLB_SLot->text();
+//    tcpData_1[277] = ui->lineEdit_LLB_Lot->text();
+//    tcpData_1[278] = ui->lineEdit_LLB_SLot->text();
+}
+
+void Widget::on_cb_ch1open_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235850";                          //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235849";                          //CEID  关闭
+    }
+    tcpData_1_2[52] = "N";   //RPTID
+    tcpData_1_2[53] = "N";   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH1_Lot->text();   //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH1_SLot->text();   //SlotID
+    tcpData_1_2[56] = "N";   //WaferID
+    tcpData_1_2[57] = "N";   //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_ch3open_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235856";  //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235855";  //CEID  关闭
+    }
+    tcpData_1_2[52] = "N";   //RPTID
+    tcpData_1_2[53] = "N";   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH3_Lot->text();   //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH3_SLot->text();   //SlotID
+    tcpData_1_2[56] = "N";   //WaferID
+    tcpData_1_2[57] = "N";   //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_ch4open_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235862";  //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235861";  //CEID  关闭
+    }
+    tcpData_1_2[52] = "N";   //RPTID
+    tcpData_1_2[53] = "N";   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH4_Lot->text();   //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH4_SLot->text();   //SlotID
+    tcpData_1_2[56] = "N";   //WaferID
+    tcpData_1_2[57] = "N";   //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_ch6open_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235868";  //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235867";  //CEID  关闭
+    }
+    tcpData_1_2[52] = "N";   //RPTID
+    tcpData_1_2[53] = "N";   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH6_Lot->text();   //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH6_SLot->text();   //SlotID
+    tcpData_1_2[56] = "N";   //WaferID
+    tcpData_1_2[57] = "N";   //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_LAopen_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235842";  //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235841";  //CEID  关闭
+    }
+    tcpData_1_2[52] = "N";   //RPTID
+    tcpData_1_2[53] = "N";   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_LLA_Lot->text();   //LotID
+    tcpData_1_2[55] = ui->lineEdit_LLA_SLot->text();   //SlotID
+    tcpData_1_2[56] = "N";   //WaferID
+    tcpData_1_2[57] = "N";   //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_LBopen_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235844";                            //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235843";                        //CEID  关闭
+    }
+    tcpData_1_2[52] = "N";                            //RPTID
+    tcpData_1_2[53] = "N";                            //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_LLB_Lot->text();   //LotID
+    tcpData_1_2[55] = ui->lineEdit_LLB_SLot->text();  //SlotID
+    tcpData_1_2[56] = "N";                            //WaferID
+    tcpData_1_2[57] = "N";                            //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_ch1start_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235847";                                   //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235848";                               //CEID  关闭
+        ui->lineEdit_CH1_StepIndex->setText("0");            //结束归零
+    }
+    tcpData_1_2[52] = "N";                                   //RPTID
+    tcpData_1_2[53] = "N";                                   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH1_Lot->text();          //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH1_SLot->text();         //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH1_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_ch3start_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235853";  //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235854";  //CEID  关闭
+        ui->lineEdit_CH3_StepIndex->setText("0");            //结束归零
+    }
+    tcpData_1_2[52] = "N";   //RPTID
+    tcpData_1_2[53] = "N";   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH3_Lot->text();   //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH3_SLot->text();   //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH3_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_ch4start_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235859";                                   //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235860";                               //CEID  关闭
+        ui->lineEdit_CH4_StepIndex->setText("0");            //结束归零
+    }
+    tcpData_1_2[52] = "N";                                   //RPTID
+    tcpData_1_2[53] = "N";                                   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH4_Lot->text();          //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH4_SLot->text();         //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH4_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_cb_ch6start_clicked(bool checked)
+{
+    tcpData_1_2[51] = "235865";                                   //CEID  默认为打开
+    if(checked == false)
+    {
+        tcpData_1_2[51] = "235866";                               //CEID  关闭
+        ui->lineEdit_CH6_StepIndex->setText("0");            //结束归零
+    }
+    tcpData_1_2[52] = "N";                                   //RPTID
+    tcpData_1_2[53] = "N";                                   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH6_Lot->text();          //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH6_SLot->text();         //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH6_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_pushButton_Ch1_StepIndex_clicked()
+{
+    tcpData_1_2[51] = "235900";                                   //CEID
+    tcpData_1_2[52] = "N";                                   //RPTID
+    tcpData_1_2[53] = "N";                                   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH1_Lot->text();          //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH1_SLot->text();         //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH1_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_pushButton_Ch3_StepIndex_clicked()
+{
+    tcpData_1_2[51] = "235903";                                   //CEID
+    tcpData_1_2[52] = "N";                                   //RPTID
+    tcpData_1_2[53] = "N";                                   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH3_Lot->text();          //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH3_SLot->text();         //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH3_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_pushButton_Ch4_StepIndex_clicked()
+{
+    tcpData_1_2[51] = "235906";                                   //CEID
+    tcpData_1_2[52] = "N";                                   //RPTID
+    tcpData_1_2[53] = "N";                                   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH4_Lot->text();          //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH4_SLot->text();         //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH4_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
+}
+
+void Widget::on_pushButton_Ch6_StepIndex6_clicked()
+{
+    tcpData_1_2[51] = "235909";                                   //CEID
+    tcpData_1_2[52] = "N";                                   //RPTID
+    tcpData_1_2[53] = "N";                                   //RecipeNameEQ
+    tcpData_1_2[54] = ui->lineEdit_CH6_Lot->text();          //LotID
+    tcpData_1_2[55] = ui->lineEdit_CH6_SLot->text();         //SlotID
+    tcpData_1_2[56] = tcpData_1_2[54]+"#"+tcpData_1_2[55];   //WaferID
+    tcpData_1_2[57] = ui->lineEdit_CH6_StepIndex->text();    //StepIndex
+    if(!m_tcpServer.getClientIds().isEmpty())
+    {
+        m_tcpServer.sendMessage(m_tcpServer.getClientIds()[0],tcpData_1_2.join(","));
+    }
 }
--- RGA-AnalogMachine/widget.h
+++ RGA-AnalogMachine/widget.h
@@ -21,6 +21,34 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+    void on_cb_ch1open_clicked(bool checked);
+
+    void on_cb_ch3open_clicked(bool checked);
+
+    void on_cb_ch4open_clicked(bool checked);
+
+    void on_cb_ch6open_clicked(bool checked);
+
+    void on_cb_LAopen_clicked(bool checked);
+
+    void on_cb_LBopen_clicked(bool checked);
+
+    void on_cb_ch1start_clicked(bool checked);
+
+    void on_cb_ch3start_clicked(bool checked);
+
+    void on_cb_ch4start_clicked(bool checked);
+
+    void on_cb_ch6start_clicked(bool checked);
+
+    void on_pushButton_Ch1_StepIndex_clicked();
+
+    void on_pushButton_Ch3_StepIndex_clicked();
+
+    void on_pushButton_Ch4_StepIndex_clicked();
+
+    void on_pushButton_Ch6_StepIndex6_clicked();
+
 private:
     TcpServer m_tcpServer;
     TcpServer m_tcpServer_2;
@@ -28,6 +56,7 @@ private:
     QTimer *m_testTimer;
     AgreementZXJC m_agreementZXJC;
     QStringList tcpData_1;
+    QStringList tcpData_1_2;
 
     void on_sendTimer_out();
     void on_testTimer_out();
